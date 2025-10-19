9bb4e60c08f3e6c6b35a694883c7b54fddaa6409
blossom-moon <158806538+blossom-moon@users.noreply.github.com>
2025-10-19 21:20:46 +0300

    Delete functions.ts

--- functions.ts
+++
@@ -1,6 +0,0 @@
-export function processData(text: string, count: number = 1): string {
-    return text.repeat(count);
-}
-
-console.log(processData("Hello")); // Hello
-console.log(processData("Hi", 3)); // HiHiHi
